3e7e558bcb3f2a1098858d51bbc382d07da63801
Vladislav Iskakov <71043551+Sophrona@users.noreply.github.com>
2023-12-01 03:02:09 +0800

    clean up

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,8 +1,3 @@
-// Подключение функционала "Чертогов Фрилансера"
-import { isMobile } from "./functions.js";
-// Подключение списка активных модулей
-import { flsModules } from "./modules.js";
-
 import "./plan-card.js";
 import './mobile-menu.js';
 import "./sliders.js";
